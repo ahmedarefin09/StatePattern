629f5dd5e379ada9e41632c8bbc84f90c30ba309
ahmedarefin09 <ahmed.arefin.09@gmail.com>
2016-12-11 20:12:12 +0700

    State pattern bad commit edited

---
+++ StatePattern/UserStateNew.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    class UserStateNew : IUserState
+    {
+        public void ChangeState(User user)
+        {
+            user.UserState = StateChanger.GetState(user);
+        }
+
+        public bool IsValidStateForTheUser(User user)
+        {
+            bool isUserIdValid = !string.IsNullOrEmpty(user.UserId);
+            bool isContactNumberValid = !string.IsNullOrEmpty(user.ContactNumber) && user.ContactNumber.Length == 5;
+
+            if (!isUserIdValid || !isContactNumberValid)
+                return true;
+
+            return false;
+        }
+
+        public void PrintInstruction()
+        {
+            Console.WriteLine("\nEnter user id and contact number. contact number should have exactly 5 digits.");
+        }
+
+        public void PrintStateDetails()
+        {
+            Console.WriteLine("User State<New>: User has not valid userid and contact number yet.");
+        }
+    }
+}
